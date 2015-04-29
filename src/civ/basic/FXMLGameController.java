12ac096516e4ca4610559bfe73046e5908d29aec
Scrubzilla <nicklas.brisendal@hotmail.com>
2015-04-29 12:36:28 +0200

    -TEST UPLOAD -Next turn button has a basic function

--- src/civ/basic/FXMLGameController.java
+++ src/civ/basic/FXMLGameController.java
@@ -8,6 +8,7 @@ package civ.basic;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -21,12 +22,23 @@ import javafx.scene.control.TextArea;
  * @author Sweetpink
  */
 public class FXMLGameController implements Initializable {
+//------------------------------VARIABLES-------------------------------------\\    
+    private double amountOfGold = 0;
+    private double amountOfWood = 0;
+    private double amountOfStone = 0;
+    private double amountOfFood = 0;
+    private double amountOfHuman = 0;
+    private double amountOfIron = 0;
+    private double amountOfCoal = 0;
+    private double amountOfSteel = 0;
+    private int currentTurn = 0;
+    
     
     @FXML
-    private Label goldLabel, woodLabel, stoneLabel, foodLabel, humanLabel, ironLabel, coalLabel, steelLabel;
+    private Label goldLabel, woodLabel, stoneLabel, foodLabel, humanLabel, ironLabel, coalLabel, steelLabel, currentTurnLabel;
     
     @FXML
-    private Button houseButton, woodmillButton, farmButton, stonemasonryButton, nextturnButton;
+    private Button houseButton, woodmillButton, farmButton, stonemasonryButton, nextTurnButton;
     
     @FXML
     private TableView resourcesTableview, buildingsTableview, statviewTableview;
@@ -39,7 +51,42 @@ public class FXMLGameController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        onNewGame();
     }    
+    
+    //------------------------------FXML METHODS----------------------------------\\
+    @FXML
+    private void nextTurn(ActionEvent event){
+        currentTurn++;
+        currentTurnLabel.setText("Current turn: " + currentTurn);
+        
+        refreshResources();
+        
+        if(currentTurn > 20){
+            DataStorage.getInstance().sceneSwitch(event, "FXMLMainMenu.fxml");
+        }
+
+    }
+    
+    //----------------------------NON-FXML METHODS--------------------------------\\
+    private void refreshResources(){
+        
+        goldLabel.setText("Gold: " + amountOfGold);
+        woodLabel.setText("Wood: " + amountOfWood);
+        stoneLabel.setText("Stone: " + amountOfStone);
+        foodLabel.setText("Food: " + amountOfFood);
+        humanLabel.setText("Human: " + amountOfHuman);
+        ironLabel.setText("Iron: " + amountOfIron);
+        coalLabel.setText("Coal: " + amountOfCoal);
+        steelLabel.setText("Steel: " + amountOfSteel);
+    }
+    
+    private void onNewGame(){
+        currentTurn++;
+        currentTurnLabel.setText("Current turn: " + currentTurn);
+        
+        refreshResources();
+        
+    }
     
 }
